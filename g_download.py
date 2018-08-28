1de5487c592dbcebe91099fef5a45734d0ba0237
root <root@DESKTOP-0CT49M1.localdomain>
2018-08-29 01:04:41 +0200

    Download modification to g_download script

--- g_download.py
+++ g_download.py
@@ -1,6 +1,6 @@
 from google_drive_downloader import GoogleDriveDownloader as gdd
 
 gdd.download_file_from_google_drive(file_id='1zIX4oGCNY_1YZjXOM068b7ghekuSmXCo',
-                                    dest_path='./data/mnist.zip',
+                                    dest_path='./mnist.zip',
                                     unzip=True)
 #https://drive.google.com/open?id=1zIX4oGCNY_1YZjXOM068b7ghekuSmXCo
