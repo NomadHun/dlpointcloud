4b73446896f5a6bbca4f369f886bab71284f7c79
root <root@DESKTOP-0CT49M1.localdomain>
2018-09-01 19:09:16 +0200

    CHanges of download archive

--- g_download.py
+++ g_download.py
@@ -1,6 +1,7 @@
 from google_drive_downloader import GoogleDriveDownloader as gdd
 
-gdd.download_file_from_google_drive(file_id='1zIX4oGCNY_1YZjXOM068b7ghekuSmXCo',
+gdd.download_file_from_google_drive(file_id='1wi-rN7-c6rNtw7ODoHrYJx2WJMFLYkRY',
                                     dest_path='./mnist.zip',
                                     unzip=True)
 #https://drive.google.com/open?id=1zIX4oGCNY_1YZjXOM068b7ghekuSmXCo
+#https://drive.google.com/open?id=1wi-rN7-c6rNtw7ODoHrYJx2WJMFLYkRY
--- train.py
+++ train.py
@@ -21,7 +21,7 @@ parser.add_argument('--gpu', type=int, default=0, help='GPU to use [default: GPU
 parser.add_argument('--log_dir', default='log', help='Log dir [default: log]')
 parser.add_argument('--num_point', type=int, default=4096, help='Point number [default: 4096]')
 parser.add_argument('--max_epoch', type=int, default=10, help='Epoch to run [default: 50]')
-parser.add_argument('--batch_size', type=int, default=44, help='Batch Size during training [default: 24]')
+parser.add_argument('--batch_size', type=int, default=6, help='Batch Size during training [default: 24]')
 parser.add_argument('--learning_rate', type=float, default=0.001, help='Initial learning rate [default: 0.001]')
 parser.add_argument('--momentum', type=float, default=0.9, help='Initial learning rate [default: 0.9]')
 parser.add_argument('--optimizer', default='adam', help='adam or momentum [default: adam]')
